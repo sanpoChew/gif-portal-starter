f4b4e9bb1043cfcf61c13b8631c97cee754e11f0
Joseph Bushell <joe@sanpochew.com>
2022-03-13 17:41:04 +0000

    feat: show user address below gif

--- app/gif-portal-starter/src/App.js
+++ app/gif-portal-starter/src/App.js
@@ -29,12 +29,6 @@ const opts = {
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const TEST_GIFS = [
-  "https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp",
-  "https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g",
-  "https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g",
-  "https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp",
-];
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null);
@@ -147,7 +141,12 @@ const App = () => {
       );
 
       console.log("Got the account", account);
-      setGifList(account.gifList);
+      setGifList(
+        account.gifList.map((gifListItem) => ({
+          ...gifListItem,
+          userAddress: new PublicKey(account.gifList[0].userAddress).toString(),
+        }))
+      );
     } catch (error) {
       console.log("Error in getGifList: ", error);
       setGifList(null);
@@ -220,6 +219,7 @@ const App = () => {
                 {gifList.map((item, index) => (
                   <div className="gif-item" key={index}>
                     <img src={item.gifLink} />
+                    <span>{item.userAddress}</span>
                   </div>
                 ))}
               </div>
